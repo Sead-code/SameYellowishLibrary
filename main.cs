cf466d1a2fc1ee7201ce6208ba29b4f856747cb6
Sead-code <seadahmesula847@gmail.com>
2024-09-07 22:30:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Robert Hall");
+    Console.WriteLine ("2/27/1949");
+    Console.WriteLine ("work 000-444-0101");
+    Console.WriteLine ("cell 000-444-0189");
+  }
+}
