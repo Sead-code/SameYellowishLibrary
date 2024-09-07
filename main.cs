1d8e94146a3ddf150a7c3725da6f7f44332fbfec
Sead-code <seadahmesula847@gmail.com>
2024-09-07 22:35:42 +0000

    Addition of Unit #3

--- main.cs
+++ main.cs
@@ -1,10 +1,12 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Robert Hall");
-    Console.WriteLine ("2/27/1949");
-    Console.WriteLine ("work 000-444-0101");
-    Console.WriteLine ("cell 000-444-0189");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Chris Johnson");
+        Console.WriteLine("12/01/1987");
+        Console.WriteLine("work 000-222-0101");
+        Console.WriteLine("cell 000-222-0189");
+    }
 }
